93f63d1b56e6d4090000a85438f10a02b822668e
Nick Campbell <nicholas.j.campbell@gmail.com>
2016-08-17 15:39:18 -0400

    Remove  from linking.
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- EditableDiv.js
+++ EditableDiv.js
@@ -142,7 +142,7 @@ module.exports = React.createClass({
 	_onLinkSubmit: function _onLinkSubmit() {
 		ReactDOM.findDOMNode(this.refs.editor).focus();
 		this._restoreSelection(this.state.textSelection);
-		this._execCommand('createLink', 'javascript:window.open(\'' + this.refs.linkInput.getInputDOMNode().value + '\');');
+		this._execCommand('createLink', this.refs.linkInput.getInputDOMNode().value);
 		this._toggleLinkTooltip();
 	},
 
